095d01b3333700938b1035beda494b9f9b285d41
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 21:16:53 +0400

    add Header component for site navigation

---
+++ components/sections/header/index.tsx
@@ -0,0 +1,17 @@
+import { TextAlignJustify } from "lucide-react";
+import Link from "next/link";
+
+const Header = ({ className }: { className?: string }) => {
+  return (
+    <header className={`absolute top-0 left-0 right-0 z-10 py-7 px-3 bg-[#BA1A33] flex items-center justify-between ${className}`}>
+      <Link href="/">
+        <img className="w-20 xs:w-full" src="/logo.svg" alt="logo" />
+      </Link>
+      <button className="text-white cursor-pointer">
+        <TextAlignJustify size={30} />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
