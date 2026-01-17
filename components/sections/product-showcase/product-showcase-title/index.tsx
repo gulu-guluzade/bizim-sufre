3231468331c82cfb3b4f48d2c6dd5783d1de6dcb
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 17:46:29 +0400

    add ProductShowcaseTitle component for product display

---
+++ components/sections/product-showcase/product-showcase-title/index.tsx
@@ -0,0 +1,9 @@
+const ProductShowcaseTitle = () => {
+    return (
+        <div className="text-center">
+            <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-semibold text-white">Products</h2>
+        </div>
+    )
+}
+
+export default ProductShowcaseTitle
