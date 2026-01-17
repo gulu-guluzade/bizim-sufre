9edc2b753ac391b59ccf220ace4496bef067ad99
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 17:50:07 +0400

    add ProductShowcase component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import HomeHero from "@/components/sections/home-hero";
+import ProductShowcase from "@/components/sections/product-showcase";
 import RecipeShowcase from "@/components/sections/recipe-showcase";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <main>
       <HomeHero />
       <RecipeShowcase />
+      <ProductShowcase />
     </main>
   );
 };
