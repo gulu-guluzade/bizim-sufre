661becbcc8a7dc69f09a281e4a54ef226aa7c803
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 21:17:03 +0400

    add Header component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/sections/header";
 import HomeHero from "@/components/sections/home-hero";
 import ProductShowcase from "@/components/sections/product-showcase";
 import RecipeShowcase from "@/components/sections/recipe-showcase";
@@ -5,6 +6,7 @@ import RecipeShowcase from "@/components/sections/recipe-showcase";
 const Home = () => {
   return (
     <main>
+      <Header className="bg-transparent"/>
       <HomeHero />
       <RecipeShowcase />
       <ProductShowcase />
