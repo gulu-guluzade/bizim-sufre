b186e45706a3d409fff5c746f1a2eee2a44fc7de
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 17:44:04 +0400

    add mayonnaiseSliderData mock for product slider functionality

---
+++ mock/mayonnaise-slider-data.ts
@@ -0,0 +1,46 @@
+import IProductSliderData from "@/types/product-slider-data";
+
+const mayonnaiseSliderData: IProductSliderData [] = [
+    {
+        id: 1,
+        title: "CLASSIC MAYONNAISE",
+        weight: "500/891ml",
+        description: "Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.",
+        imageUrl: "product-slider-mayonnaise/1.png",
+        productCategory: "mayonnaise",
+    },
+    {
+        id: 2,
+        title: "MAYONNAISE WITH SOUR CREAM TASTE",
+        weight: "200/400/800 ml",
+        description: "We didn’t disappoint those who love both mayonnaise and sour cream in salad and dishes. We created “Bizim Sufre” mayonnaise with Sour Cream for them. Two main indispensable sauce of kitchens together – In “Bizim Süfrə” jar! It gives taste and delicacy to any food with its taste and puerility.",
+        imageUrl: "/product-slider-mayonnaise/2.png",
+        productCategory: "mayonnaise",
+    },
+    {
+        id: 3,
+        title: "CLASSIC MAYONNAISE",
+        weight: "200/400 ml",
+        description: "Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.",
+        imageUrl: "/product-slider-mayonnaise/3.png",
+        productCategory: "mayonnaise",
+    },
+    {
+        id: 4,
+        title: "MAYONNAISE WITH SOUR CREAM TASTE",
+        weight: "891 ml",
+        description: "We didn’t disappoint those who love both mayonnaise and sour cream in salad and dishes. We created “Bizim Sufre” mayonnaise with Sour Cream for them. Two main indispensable sauce of kitchens together – In “Bizim Süfrə” jar! It gives taste and delicacy to any food with its taste and puerility.",
+        imageUrl: "/product-slider-mayonnaise/4.png",
+        productCategory: "mayonnaise",
+    },
+    {
+        id: 5,
+        title: "LOW CALORIE MAYONNAISE",
+        weight: "200/400 ml",
+        description: "Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.",
+        imageUrl: "/product-slider-mayonnaise/5.png",
+        productCategory: "mayonnaise",
+    },
+];
+
+export default mayonnaiseSliderData;
