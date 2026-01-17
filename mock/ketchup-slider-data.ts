82f0da2268baf0837378d18472bd7125d88def36
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-01-17 17:43:54 +0400

    add ketchupSliderData mock for product slider functionality

---
+++ mock/ketchup-slider-data.ts
@@ -0,0 +1,46 @@
+import IProductSliderData from "@/types/product-slider-data";
+
+const ketchupSliderData: IProductSliderData [] = [
+    {
+        id: 1,
+        title: "CLASSIC KETCHUP",
+        weight: "200/350/500q",
+        description: "Bizim Sufre sauce cooked ketchup is made from sun-drenched tomatoes to give your dishes such as food, pastes, pizzas, pastries and more an extra taste. “Bizim Sufre” classic ketchup contains protein, fat and carbohydrate, so you can make fresh sauces with it. This is the best solution of forcing children to love meals.",
+        imageUrl: "/product-slider-ketchup/1.png",
+        productCategory: "ketchup",
+    },
+    {
+        id: 2,
+        title: "HOT KETCHUP",
+        weight: "200/350/500q",
+        description: "The spicy assortment of “Bizim Sufre” ketchup, which will turn into a delicious detail of your dishes, has own taste due to your palatal-delight. Hamburgers, toasts, any kind of fries, pastes, pizza and other products are the perfect companion to your dishes and has own place on your tables.",
+        imageUrl: "/product-slider-ketchup/2.png",
+        productCategory: "ketchup",
+    },
+    {
+        id: 3,
+        title: "HOT KETCHUP",
+        weight: "350q",
+        description: "The spicy assortment of “Bizim Sufre” ketchup, which will turn into a delicious detail of your dishes, has own taste due to your palatal-delight. Hamburgers, toasts, any kind of fries, pastes, pizza and other products are the perfect companion to your dishes and has own place on your tables.",
+        imageUrl: "/product-slider-ketchup/3.png",
+        productCategory: "ketchup",
+    },
+    {
+        id: 4,
+        title: "CLASSIC KETCHUP",
+        weight: "540q",
+        description: "Bizim Sufre sauce cooked ketchup is made from sun-drenched tomatoes to give your dishes such as food, pastes, pizzas, pastries and more an extra taste. “Bizim Süfrə” classic ketchup contains protein, fat and carbohydrate, so you can make fresh sauces with it. This is the best solution of forcing children to love meals.",
+        imageUrl: "/product-slider-ketchup/4.png",
+        productCategory: "ketchup",
+    },
+    {
+        id: 5,
+        title: "HOT KETCHUP",
+        weight: "680q",
+        description: "The spicy assortment of “Bizim Sufre” ketchup, which will turn into a delicious detail of your dishes, has own taste due to your palatal-delight. Hamburgers, toasts, any kind of fries, pastes, pizza and other products are the perfect companion to your dishes and has own place on your tables.",
+        imageUrl: "/product-slider-ketchup/5.png",
+        productCategory: "ketchup",
+    },
+];
+
+export default ketchupSliderData;
