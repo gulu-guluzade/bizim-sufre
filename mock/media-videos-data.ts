57d1c0463dbdc38f8a708d409ab4a6554c90d08c
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-02-16 02:33:07 +0400

    videos data added

---
+++ mock/media-videos-data.ts
@@ -0,0 +1,18 @@
+import IVideos from '@/types/media-videos';
+
+const videos: IVideos[] = [
+  { id: 'a7Ncz1S2bNk' },
+  { id: 'ko4XJ4D7gJ8' },
+  { id: 'JVeei66NDIo' },
+  { id: 'zV6_MQFrijE' },
+  { id: 'd5Ul_J4bYfc' },
+  { id: 'nXOnaSRImbg' },
+  { id: 'kmbiPo3ZoyQ' },
+  { id: 'Fw1oA-5ax1Y' },
+  { id: 'h8FIQBZJpz8' },
+  { id: 'AuJx7EfjcfE' },
+  { id: 'vvUw9mJ0ueM' },
+  { id: 'FFaaaAoKbzk' },
+];
+
+export default videos;
