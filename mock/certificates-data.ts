b63a5dccf932861f5c6b799ba86c17c70b7e2cf5
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-02-16 01:18:04 +0400

    certificates data added

---
+++ mock/certificates-data.ts
@@ -0,0 +1,96 @@
+import ICertificatesData from '@/types/certificate';
+
+const certificateData: ICertificatesData[] = [
+  {
+    id: 1,
+    title: 'Bizim Süfrə - sarımsaqlı sous',
+    date: '11 Sentyabr, 2024 - 10 Sentyabr 2025',
+    imgSrc: '/certificates/1.jpg',
+  },
+  {
+    id: 2,
+    title: 'TSE TS ISO 45001',
+    date: '18 Aprel, 2024 - 18 Aprel, 2027',
+    imgSrc: '/certificates/2.jpg',
+  },
+  {
+    id: 3,
+    title: 'TSE TS ISO 45001',
+    date: '18 Aprel, 2024',
+    imgSrc: '/certificates/3.jpg',
+  },
+  {
+    id: 4,
+    title: 'TS ISO 45001',
+    date: '18 Aprel, 2024 - 18 Aprel, 2027',
+    imgSrc: '/certificates/4.jpg',
+  },
+  {
+    id: 5,
+    title: 'TS ISO 45001',
+    date: '',
+    imgSrc: '/certificates/5.jpg',
+  },
+  {
+    id: 6,
+    title: 'Bizim süfrə - ketçup',
+    date: '17 Yanvar, 2025 - 15 Yanvar, 2026',
+    imgSrc: '/certificates/6.jpg',
+  },
+  {
+    id: 7,
+    title: 'K - Q TSE-ISO-EN 9001',
+    date: '12 Dekabr, 2024',
+    imgSrc: '/certificates/7.jpg',
+  },
+  {
+    id: 8,
+    title: 'K - Q TSE-ISO-EN 9001',
+    date: '12 Dekabr, 2024 - 12 Dekabr, 2027',
+    imgSrc: '/certificates/8.jpg',
+  },
+  {
+    id: 9,
+    title: 'TSE FSSC 22000 ISO/TS 22002-1',
+    date: '4 Dekabr, 2023',
+    imgSrc: '/certificates/9.jpg',
+  },
+  {
+    id: 10,
+    title: 'TSE FSSC 22000 ISO/TS 22002-1',
+    date: '4 Dekabr, 2023 - 16 Mart, 2027',
+    imgSrc: '/certificates/10.jpg',
+  },
+  {
+    id: 11,
+    title: 'Bizim Süfrə - Burger sousu',
+    date: '6 İyun, 2024 - 4 İyun, 2025',
+    imgSrc: '/certificates/11.jpg',
+  },
+  {
+    id: 12,
+    title: 'Bizim Süfrə - Mayonez',
+    date: '11 Sentyabr, 2024 - 10 Sentyabr, 2025',
+    imgSrc: '/certificates/12.jpg',
+  },
+  {
+    id: 13,
+    title: 'Bizim Süfrə - Barbeku sousu',
+    date: '6 İyun, 2024 - 4 İyun, 2025',
+    imgSrc: '/certificates/13.jpg',
+  },
+  {
+    id: 14,
+    title: 'Bizim Süfrə - Sezar sousu',
+    date: '6 İyun, 2024 - 4 İyun, 2025',
+    imgSrc: '/certificates/14.jpg',
+  },
+  {
+    id: 15,
+    title: 'Akkreditasiya Attestatı',
+    date: '8 İyul, 2024 - 8 İyul, 2027',
+    imgSrc: '/certificates/15.jpg',
+  },
+];
+
+export default certificateData;
