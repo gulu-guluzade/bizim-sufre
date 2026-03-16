ccc8e5e5a8a9573ff9df19f769fa54a78efbde5d
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-03-16 08:31:28 +0400

    mayonnaise data created

---
+++ mock/products-mayonnaise.ts
@@ -0,0 +1,176 @@
+import { IProducts } from '@/types/products';
+
+const productsMayonnaise: IProducts[] = [
+  {
+    id: '1',
+    title: 'PROVENCE MAYONNAISE',
+    imgSrc: '/products/mayonnaise/1.png',
+    desc: 'This product has been prepared in accordance with old traditions in order to preserve the traditional taste of Provencal mayonnaise having 67% fat content. The heart of the various salads on the tables we lay on holidays and special occasions beats with our Provencal mayonnaise. As well, there is no second taste suiting your fries like this mayonnaise.',
+    ingredients:
+      'sunflower oil, pasteurized water, granulated sugar, stabilizing complex: egg yolk powder, pectin fibers, guar gum (E412), xanthan gum (E415), dextrose, maltodextrin, salt, vinegar, acidity regulator: citric acid (E330), preservatives: potassium sorbate (E202), antioxidant: EDTA (E385), mustard oil,',
+    totalWeight: '200 / 400 ml',
+    totalValuePer100gr: {
+      fat: '67.52 q',
+      protein: '0.31 q',
+      carbohydrate: '3.4 q',
+      energy: '2604.62  kC/ 622.52 kkal',
+      innerPiece: '24 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO  free.',
+  },
+  {
+    id: '2',
+    title: 'OLIVE MAYONNAISE',
+    imgSrc: '/products/mayonnaise/2.png',
+    desc: 'Useful product such as olives was used to add flavor to the delicious "Bizim Sufre" mayonnaise. You can prepare unique salads and snacks in your kitchen using this olive mayonnaise. Each of your guest will taste delicious meal served with olive mayonnaise having convenient squeezing feature with your hot dishes. Bizim Sufre olive mayonnaise will become a permanent resident of your daily table after one use.',
+    ingredients:
+      'winterized and deodorized sunflower oil, refined olive oil, pasteurized water, stabilizing complex: egg yolk powder, guar gum (E412), xanthan gum (E415), starch (E1422), granulated sugar, salt, vinegar, acidity regulator: milk acid (E270), citric acid (E330), preservatives: potassium sorbate (E202), antioxidant: EDTA (E385), mustard oil, beta-carotene (E160a)',
+    totalWeight: '200/400/800 ml',
+    totalValuePer100gr: {
+      fat: '55.52 q',
+      protein: '0.3 q',
+      carbohydrate: '2.98 q',
+      energy: '2144.36  kC/ 512.76 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Contains Omega 6 and Omega 9.',
+  },
+  {
+    id: '3',
+    title: 'PROVENCE MAYONNAISE',
+    imgSrc: '/products/mayonnaise/3.png',
+    desc: 'Sunflower oil, pasteurized water, sugar, stabilizer complex',
+    ingredients:
+      'Sunflower oil, pasteurized water, sugar, stabilizer complex: yolk powder, pectin fiber, guar gum (E412), xanthan gum (E415), salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), mustard oil, colorant: beta-carotene (E160a). ',
+    totalWeight: '500/891 m',
+    totalValuePer100gr: {
+      fat: '67.52 q',
+      protein: '0.31 q',
+      carbohydrate: '3.4 q',
+      energy: '2604.62  kC/ 622.52 kkal',
+      innerPiece: '6 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays.',
+  },
+  {
+    id: '4',
+    title: 'CLASSIC MAYONNAISE',
+    imgSrc: '/products/mayonnaise/4.png',
+    desc: 'Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.',
+    ingredients:
+      'Sunflower oil, pasteurized water, sugar, stabilizer complex: yolk powder, pectin fiber, guar gum (E412), xanthan gum (E415), salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), mustard oil.',
+    totalWeight: '800ml',
+    totalValuePer100gr: {
+      fat: '55.6 q',
+      protein: '0.44 q',
+      carbohydrate: '2.7 q',
+      energy: '2143 kC / 512 kkal',
+      innerPiece: '12 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. ',
+  },
+  {
+    id: '5',
+    title: 'MAYONNAISE',
+    imgSrc: '/products/mayonnaise/5.png',
+    desc: 'Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.',
+    ingredients:
+      'Sunflower oil, pasteurized water, sugar, stabilizer complex: yolk powder, pectin fiber, guar gum (E412), xanthan gum (E415), salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), mustard oil.',
+    totalWeight: '900ml',
+    totalValuePer100gr: {
+      fat: '67.52 q',
+      protein: '0.31 q',
+      carbohydrate: '3.4 q',
+      energy: '2604.62 kC / 622.52 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. ',
+  },
+  {
+    id: '6',
+    title: 'MAYONNAISE',
+    imgSrc: '/products/mayonnaise/6.png',
+    desc: 'Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.',
+    ingredients:
+      'Sunflower oil, pasteurized water, sugar, stabilizer complex: yolk powder, pectin fiber, guar gum (E412), xanthan gum (E415), salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), mustard oil.',
+    totalWeight: '4/8 kq',
+    totalValuePer100gr: {
+      fat: '67.52 q',
+      protein: '0.31 q',
+      carbohydrate: '3.4 q',
+      energy: '2604.62 kC / 622.52 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. ',
+  },
+  {
+    id: '7',
+    title: 'LOW CALORIE MAYONNAISE',
+    imgSrc: '/products/mayonnaise/7.png',
+    desc: 'Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.',
+    ingredients:
+      'Pasteurized water, sunflower oil, yolk powder, stabilizer complex: guar gum (E412), xanthan gum (E415), silicium dioxide (E551), starch (E1422), sugar, vinegar, salt, preservative: potassium sorbate (E202), acidity regulator: lactic acid (E270) and citric acid (E330), antioxidant: EDTA (E385), mustard oil, colorant: beta-carotene (E160a).',
+    totalWeight: '200/400 ml',
+    totalValuePer100gr: {
+      fat: '28.27 q',
+      protein: '0.17 q',
+      carbohydrate: '6.73 q',
+      energy: '1179.37 kC / 282.01 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. Do not buy jars with convex lid. ',
+  },
+  {
+    id: '8',
+    title: 'MAYONNAISE WITH SOUR CREAM TASTE',
+    imgSrc: '/products/mayonnaise/8.png',
+    desc: 'We didn’t disappoint those who love both mayonnaise and sour cream in salad and dishes. We created “Bizim Sufre” mayonnaise with Sour Cream for them. Two main indispensable sauce of kitchens together – In “Bizim Süfrə” jar! It gives taste and delicacy to any food with its taste and puerility.',
+    ingredients:
+      'Refined, vinterized and deodorized sunflower oil, pasteurized water, yolk powder, stabilizer complex: guar gum (E412), xanthan gum (E415), silicium dioxide (E551), starch (E1422), sugar, salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), natural ingredient of sour cream, colorant: beta-carotene (E160a).',
+    totalWeight: '891 ml',
+    totalValuePer100gr: {
+      fat: '55.52 q',
+      protein: '0.29 q',
+      carbohydrate: '6.39 q',
+      energy: '2201.15 kC / 526.34 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. Do not buy jars with convex lid.',
+  },
+  {
+    id: '9',
+    title: 'CLASSIC MAYONNAISE',
+    imgSrc: '/products/mayonnaise/9.png',
+    desc: 'Bizim Sufre mayonnaise is rich with protein, salt and fiber, but low in calories. You can easily use in all kinds of fried dishes. Moreover, you can make other salad and snacks tastier with “Bizim Süfrə” mayonnaise.',
+    ingredients:
+      'Sunflower oil, pasteurized water, sugar, stabilizer complex: yolk powder, pectin fiber, guar gum (E412), xanthan gum (E415), salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), mustard oil.',
+    totalWeight: '200/400 ml',
+    totalValuePer100gr: {
+      fat: '67.52 q',
+      protein: '0.31 q',
+      carbohydrate: '3.4 q',
+      energy: '2604.62 kC / 622.52 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. ',
+  },
+  {
+    id: '10',
+    title: 'MAYONNAISE WITH SOUR CREAM TASTE',
+    imgSrc: '/products/mayonnaise/10.png',
+    desc: 'We didn’t disappoint those who love both mayonnaise and sour cream in salad and dishes. We created “Bizim Sufre” mayonnaise with Sour Cream for them. Two main indispensable sauce of kitchens together – In “Bizim Süfrə” jar! It gives taste and delicacy to any food with its taste and puerility.',
+    ingredients:
+      'Refined, vinterized and deodorized sunflower oil, pasteurized water, yolk powder, stabilizer complex: guar gum (E412), xanthan gum (E415), silicium dioxide (E551), starch (E1422), sugar, salt, vinegar, acidity regulator: citric acid (E330), preservative: potassium sorbate (E202), antioxidant : EDTA (E385), natural ingredient of sour cream, colorant: beta-carotene (E160a).',
+    totalWeight: '200/400/800 ml',
+    totalValuePer100gr: {
+      fat: '55.52 q',
+      protein: '0.29 q',
+      carbohydrate: '6.39 q',
+      energy: '2201.15 kC / 526.34 kkal',
+      innerPiece: '40 ədəd',
+    },
+    note: 'Product of Azerbaijan. GMO free. Keep away from sunrays. Do not buy jars with convex lid. ',
+  },
+];
+
+export default productsMayonnaise;
