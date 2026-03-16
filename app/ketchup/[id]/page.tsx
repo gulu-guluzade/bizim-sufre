d404653184e75c60995133109dfdf41fa496c91b
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-03-16 08:35:31 +0400

    ketchup detail page created

---
+++ app/ketchup/[id]/page.tsx
@@ -0,0 +1,8 @@
+import productsKetchup from '@/mock/products-ketchup';
+import ProductDetailPage from '@/components/product-detail-page';
+
+const KetchupProductDetailPage = () => {
+  return <ProductDetailPage products={productsKetchup} />;
+};
+
+export default KetchupProductDetailPage;
