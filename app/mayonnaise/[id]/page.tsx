3cd53dfd385dbb81492b5b1e5987f10d19b8db00
gulu-guluzade <mr.guluzade.official@gmail.com>
2026-03-16 08:36:01 +0400

    mayonnaise detail page created

---
+++ app/mayonnaise/[id]/page.tsx
@@ -0,0 +1,8 @@
+import productsMayonnaise from '@/mock/products-mayonnaise';
+import ProductDetailPage from '@/components/product-detail-page';
+
+const MayonnaiseProductDetailPage = () => {
+  return <ProductDetailPage products={productsMayonnaise} />;
+};
+
+export default MayonnaiseProductDetailPage;
